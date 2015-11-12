9595e9683b71aa0afbc2825437f6fbd21765cbef
victorclf <victorclf@live.com>
2015-11-12 09:25:42 -0300

    Add Mergely lib for displaying code and diff-regions

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-	$('#jstree_partition_div').jstree({
+	$('#partition_tree').jstree({
 		'core' : {
 			'data' : {
 				"url" : "./test/examples/toy_example/partitions.json",
 				"dataType" : "json"
 			}
+		}
+	});
+	
+	$('#partition_tree').on("loaded.jstree", function () {
+		$('#partition_tree').jstree().open_all();
+	});
+	
+	
+	$('#code_editor').mergely({
+		cmsettings: { readOnly: false, lineNumbers: true },
+		lhs: function(setValue) {
+			setValue('the quick red fox\njumped over the hairy dog');
+		},
+		rhs: function(setValue) {
+			setValue('the quick brown fox\njumped over the lazy dog');
 		}
 	});
 });
