f57f5f50cb3d73af64d0514dfab39cb3b3832d61
victorclf <victorclf@live.com>
2015-11-12 10:49:13 -0300

    Add AJAX call to populate editor with changed source file

--- main.js
+++ main.js
@@ -12,14 +12,31 @@ $(document).ready(function() {
 		$('#partition_tree').jstree().open_all();
 	});
 	
+	$('#partition_tree').on("select_node.jstree", function (node, selected, event) {
+		var text = selected.node.text;
+		if (text.endsWith(".java")) {
+			$.get(selected.node.original.before_file, function (data) {
+				$('#code_editor').mergely('lhs', data);
+			}, "text");
+			
+			$.get(selected.node.original.after_file, function (data) {
+				$('#code_editor').mergely('rhs', data);
+			}, "text");
+		}
+		
+		//~ if (text.endsWith("]")) {
+			//~ $('#code_editor').mergely('scrollTo', 'rhs', selected.node.original.line_start);
+		//~ }
+	});
+	
 	
 	$('#code_editor').mergely({
 		cmsettings: { readOnly: false, lineNumbers: true },
 		lhs: function(setValue) {
-			setValue('the quick red fox\njumped over the hairy dog');
+			setValue('No diff-region selected.');
 		},
 		rhs: function(setValue) {
-			setValue('the quick brown fox\njumped over the lazy dog');
+			setValue('No diff-region selected.');
 		}
 	});
 });
